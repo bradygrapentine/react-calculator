a5cc3a755ac3ddacef78074a733823d6d84dd64e
bradygrapentine <brady.grapentine@gmail.com>
2021-06-04 14:29:36 -0400

    html and css done, user input is displaying but no operations are functional yet

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # React Calculator
 
 ![SDG](./docs/button.png)
+
+Practice is key to gaining the mileage needed to conquer programming. A very common practice problem is to recreate a working calculator. A basic calculator is a good exercise in using state, effects, and components to create a powerful and robust UI.
+
+Objectives
+
+- Practice using functional components with hooks
+
+- Practice implementing a UI wire-frame
+
+Requirements
+
+Create a simple calculator that allows the user to add, multiply, subtract, and divide two numbers. Here is a sample UI:
+
+Notes
+Take this in small steps just like we did in class. Start with the HTML, then CSS, then with basic functionality.
+
+Explorer Mode
+
+    - Implement something similar to the example UI using HTML and CSS inside a React app
+
+<!-- DONE -->
+
+    - As a user, I should be able to add two numbers
+
+    - As a user, I should be able to subtract numbers
+
+    - As a user, I should be able to add multiple numbers
+
+    - As a user, I should be able to divide numbers
+
+    - The clear button should reset the current calculation
+
+Adventure Mode
+
+    - Show the user their history in a list
+    Be able to chain together results. For example, the user should be able to input 2 + 3 - 4 + 5 and get the correct answer
+
+    - Let the user use their keyboard in addition to the mouse
+
+Epic Mode
+
+    - Using your systems calculator app as inspiration, implement as many features as you can, including, but not limited to scientific mode, exponents, and the order of operations.
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,102 @@
-import React from 'react'
+import { React, useState } from 'react'
+// import { Buttons } from './components/Buttons'
 
 export function App() {
-  return <div>Hello, World!</div>
+  let [current, setCurrent] = useState('0')
+  let [base, setBase] = useState(0)
+
+  function setNum(event) {
+    setCurrent(
+      current === '0'
+        ? event.target.textContent === '.'
+          ? (current += event.target.textContent)
+          : event.target.textContent
+        : (current += event.target.textContent)
+    )
+  }
+
+  function clickOperation(event) {
+    switch (event.target.textContent) {
+      case '+':
+        setBase((base += parseFloat(current)))
+        break
+      case '&minus;':
+        setBase((base += parseFloat(current)))
+        break
+      case '&times;':
+        setBase((base += parseFloat(current)))
+        break
+      case '&divide;':
+        break
+      case '=':
+        break
+    }
+  }
+
+  function clearAll() {
+    setCurrent('0')
+  }
+
+  return (
+    <>
+      <main>
+        <div className="calculator">
+          <div className="display">{current}</div>
+          <div className="buttons">
+            <button onClick={clearAll} className="button clear">
+              AC
+            </button>
+            <button onClick={clearAll} className="button divide">
+              &divide;
+            </button>
+            <button onClick={setNum} className="button seven">
+              7
+            </button>
+            <button onClick={setNum} className="button eight">
+              8
+            </button>
+            <button onClick={setNum} className="button nine">
+              9
+            </button>
+            <button onClick={clearAll} className="button multiply">
+              &times;
+            </button>
+            <button onClick={setNum} className="button four">
+              4
+            </button>
+            <button onClick={setNum} className="button five">
+              5
+            </button>
+            <button onClick={setNum} className="button six">
+              6
+            </button>
+            <button onClick={clearAll} className="button minus">
+              &minus;
+            </button>
+            <button onClick={setNum} className="button one">
+              1
+            </button>
+            <button onClick={setNum} className="button two">
+              2
+            </button>
+            <button onClick={setNum} className="button three">
+              3
+            </button>
+            <button onClick={setNum} className="button plus">
+              +
+            </button>
+            <button onClick={setNum} className="button zero">
+              0
+            </button>
+            <button onClick={setNum} className="button decimal">
+              .
+            </button>
+            <button onClick={clearAll} className="button equals">
+              =
+            </button>
+          </div>
+        </div>
+      </main>
+    </>
+  )
 }
